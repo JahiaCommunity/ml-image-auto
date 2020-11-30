d518cd7a1bdb3784dfc755fa50be3ba9dae1b17d
C3Dr1c Ph0Urn34U <cedfou@gmail.com>
2020-11-30 16:34:01 +0100

    Update Constants.java

--- src/main/java/org/jahiacommunity/ml/image/utils/Constants.java
+++ src/main/java/org/jahiacommunity/ml/image/utils/Constants.java
@@ -2,6 +2,6 @@ package org.jahiacommunity.ml.image.utils;
 
 public class Constants {
     // <snippet_creds>
-    public static String subscriptionKey = "55983badbe3c40a8857b8f88c4bed7e1";  //"<your-subscription-key>";
+    public static String subscriptionKey = "PUT_YOUR_OWN_KEY";  //"<your-subscription-key>";
     public static String endpoint = "https://jahiaazure.cognitiveservices.azure.com/"; //"<your-api-endpoint>";
 }
